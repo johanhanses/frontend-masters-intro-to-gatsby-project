076b4d1d22474570b751db8ea81887ba0c689b82
Johan Hanses <johanhanses@gmail.com>
2022-01-07 18:17:16 +0100

    add and use a layout component

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,6 @@ module.exports = {
     title: "Frontend Masters Intro To Gatsby",
     description: "Frontend Masters Intro to Gatsby course project",
     image: "https://picsum.photos/id/1/200/300"
-  }
+  },
+  plugins: ["gatsby-plugin-react-helmet"]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,10 @@
 import * as React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import Layout from "../components/layout.js"
 
 export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query GetSiteTitle {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  console.log(data)
-  const meta = data?.site?.siteMetadata ?? {}
-
   return (
-    <>
-      <header>
-        <Link to="/">{meta.title}</Link>
-      </header>
-
-      <main>
-        <h1>Hello Ninjan</h1>
-        <Link to="/about">About this site</Link>
-      </main>
-    </>
+    <Layout>
+      <h1>Hello Ninjan</h1>
+    </Layout>
   )
 }
