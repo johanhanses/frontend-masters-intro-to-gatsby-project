4bf6c4674733f102c9687ee28ac8017ee7360de4
Johan Hanses <johanhanses@gmail.com>
2022-01-07 15:39:45 +0100

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://yourdomain.tld",
+    title: "Frontend Masters Intro To Gatsby",
+    description: "Frontend Masters Intro to Gatsby course project",
+    image: "https://picsum.photos/id/1/200/300"
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,30 @@
-import * as React from 'react';
+import * as React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  console.log(data)
+  const meta = data?.site?.siteMetadata ?? {}
+
   return (
-    <main>
-      <h1>Hello Ninjan</h1>
-    </main>
-  );
+    <>
+      <header>
+        <Link to="/">{meta.title}</Link>
+      </header>
+
+      <main>
+        <h1>Hello Ninjan</h1>
+        <Link to="/about">About this site</Link>
+      </main>
+    </>
+  )
 }
